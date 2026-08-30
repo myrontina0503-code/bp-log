ecbdf0aa6b5596bb4ceadeca89ca01a7368e3a98
myrontina0503-code <myrontina0503@gmail.com>
2026-08-30 18:48:27 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+/* 血壓記錄 PWA service worker:一律繞過 HTTP 快取直接問網路拿最新版,離線才用本地快取 */
+const CACHE = 'bp-log-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
+
+async function refreshCache() {
+  const c = await caches.open(CACHE);
+  await Promise.all(ASSETS.map(async (url) => {
+    try {
+      const res = await fetch(url, { cache: 'no-store' });
+      await c.put(url, res);
+    } catch (e) { /* 離線安裝時忽略,舊快取(若有)仍可用 */ }
+  }));
+}
+
+self.addEventListener('install', e => {
+  e.waitUntil(refreshCache());
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const freshReq = new Request(e.request.url, { cache: 'no-store' });
+  e.respondWith(
+    fetch(freshReq)
+      .then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      })
+      .catch(() =>
+        caches.match(e.request).then(r => r || caches.match('./index.html'))
+      )
+  );
+});
